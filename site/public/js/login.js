cc00dbe36d6874fa8d5ca5fc4b7d45fcb20bc388
GabrielaKubo <gabriela.kubo@sptech.school>
2022-06-02 15:25:33 -0300

    backend + cadastro e login

--- site/public/js/login.js
+++ site/public/js/login.js
@@ -18,7 +18,7 @@ function entrar() {
             resposta.json().then(json => {
                 console.log(json);
                 console.log(JSON.stringify(json));
-
+                sessionStorage.EMAIL_USUARIO = json.email;
                 sessionStorage.LOGIN_USUARIO = json.login;
                 sessionStorage.NOME_USUARIO = json.nome;
                 sessionStorage.ID_USUARIO = json.id;
