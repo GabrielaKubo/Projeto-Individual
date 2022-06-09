83f3cc3369ebdb852f24d74c76ed1cffb4965925
GabrielaKubo <gabriela.kubo@sptech.school>
2022-06-09 13:55:26 -0300

    nova estrutura de minigame

--- final/site/src/database/config.js
+++ final/site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "ma",
-    user: "Gabriela",
-    password: "aluno",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
