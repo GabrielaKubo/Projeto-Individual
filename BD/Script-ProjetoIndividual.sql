ba819ab983e590cff1f989d35174f7a7820800aa
GabrielaKubo <gabriela.kubo@sptech.school>
2022-06-09 22:06:55 -0300

    PRONTO!

--- BD/Script-ProjetoIndividual.sql
+++ BD/Script-ProjetoIndividual.sql
@@ -1,54 +1,59 @@
 CREATE DATABASE ma;
 USE ma;
-CREATE TABLE Usuario(
-idUsuario int primary key auto_increment,
-nomeUser VARCHAR(50),
-email VARCHAR(20),
-senha VARCHAR(20),
-fkFavoritos INT,
-foreign key (fkFavoritos) references Favoritos (idFavorito)  
-);
-
-CREATE TABLE Favoritos(
-idFavorito INT auto_increment,
-nomeFilme VARCHAR (50),
-nomepersFAV VARCHAR(50),
-fkFilmes INT,
-foreign key (fkFilmes) references Filmes(idFilmes),
-primary key (idFavorito, fkFilmes)
-);
-
 CREATE TABLE Filmes(
 idFilmes int primary key auto_increment,
 nomeFilme VARCHAR(50),
 dtLancamento INT,
 diretor VARCHAR(100)
 );
-drop table Filmes;
-INSERT INTO Filmes VALUES
-(null, 'Nausicaä do Vale do Vento', '1984', 'Hayao Miyazaki'),
-(null, 'O Castelo no Céu', '1986', 'Hayao Miyazaki'),
-(null, 'Meu Amigo Totoro', '1988', 'Hayao Miyazaki'),
-(null, 'Túmulo dos Vagalumes', '1988', 'Hayao Miyazaki'),
-(null, 'O Serviço de Entregas da Kiki', '1989', 'Hayao Miyazaki'),
-(null, 'Porco Rosso: O Último Herói Romântico', '1992', 'Hayao Miyazaki'),
-(null, 'Princesa Mononoke', '1997', 'Hayao Miyazaki'),
-(null, 'A Viagem de Chihiro', '2001', 'Hayao Miyazaki'),
-(null, 'O Castelo Animado', '2004', 'Hayao Miyazaki'),
-(null, 'Ponyo: Uma Amizade Que Veio do Mar ', '2008', 'Hayao Miyazaki'),
-(null, 'Vidas ao Vento', '2013', 'Hayao Miyazaki');
-TRUNCATE table Filmes;
-select * from Filmes;
- CREATE TABLE Personagens(
+
+CREATE TABLE Personagens(
  idPersonagem int auto_increment,
  nomePersonagem VARCHAR(50),
- genero char(10),
+ genero VARCHAR(50),
  constraint chkgen CHECK (genero in('feminino', 'masculino','criatura mágica')),
  ax_filmes INT,
  foreign key (ax_filmes) references Filmes(idfilmes),
  primary key (idPersonagem, ax_filmes)
  );
- drop table Personagens;
+ 
+CREATE TABLE Distribuidora(
+idDis int primary key auto_increment,
+nomeDis VARCHAR(50),
+receita VARCHAR(20),
+ay_filme INT,
+foreign key (ay_Filme) references Filmes (idFilmes)  
+);
+
+CREATE TABLE Usuario(
+idUsuario INT auto_increment primary key,
+nomeUser VARCHAR (50),
+email VARCHAR(50),
+senha VARCHAR(20),
+fkFilmes INT,
+foreign key (fkFilmes) references Filmes(idFilmes),
+fkPersonagem INT,
+foreign key (fkPersonagem) references Personagens(idPersonagem)
+);
+ 
+
+UPDATE Usuario SET fkFilmes = 1  WHERE idUsuario = 1;
+UPDATE Usuario SET fkPersonagem = 2 WHERE idUsuario = 1;
+ 
+INSERT INTO Filmes VALUES
+(null, 'Nausicaä', '1984', 'Hayao Miyazaki'),
+(null, 'O Castelo no Céu', '1986', 'Hayao Miyazaki'),
+(null, 'Meu Amigo Totoro', '1988', 'Hayao Miyazaki'),
+(null, 'Túmulo dos Vagalumes', '1988', 'Hayao Miyazaki'),
+(null, 'O Serviço de Entregas da Kiki', '1989', 'Hayao Miyazaki'),
+(null, 'Porco Rosso', '1992', 'Hayao Miyazaki'),
+(null, 'Princesa Mononoke', '1997', 'Hayao Miyazaki'),
+(null, 'A Viagem de Chihiro', '2001', 'Hayao Miyazaki'),
+(null, 'O Castelo Animado', '2004', 'Hayao Miyazaki'),
+(null, 'Ponyo', '2008', 'Hayao Miyazaki'),
+(null, 'Vidas ao Vento', '2013', 'Hayao Miyazaki');
+ 
+
  INSERT INTO Personagens VALUES
  (null, 'Kushana', 'feminino', 1),
  (null, 'Nausicaa', 'feminino', 1),
@@ -62,22 +67,87 @@ select * from Filmes;
  (null, 'Mei Kusakabe', 'feminino', 3),
  (null, 'Satsuki Kusakabe', 'feminino', 3),
  (null, 'Catbus', 'criatura mágica', 3),
- (null, 'Jiji', 'criatura mágica', 4),
- (null, 'Tombo', 'masculino', 4),
- (null, 'Ursula', 'feminino', 4),
- (null, 'Kiki', 'feminino', 4),
- (null, 'Gina', 'feminino', 5),
- (null, 'Porco Rosso', 'masculino', 5),
- (null, 'Fio Piccolo', 'feminino', 5),
- (null, 'Nausicaa', 'masculino', 9),
- (null, 'Nausicaa', 'masculino', 9),
- (null, 'Nausicaa', 'masculino', 9),
- (null, 'Nausicaa', 'masculino', 9),
- (null, 'Nausicaa', 'masculino', 10),
- (null, 'Nausicaa', 'masculino', 10),
- (null, 'Nausicaa', 'masculino', 10),
- (null, 'Nausicaa', 'masculino', 10),
- (null, 'Nausicaa', 'masculino', 11),
- (null, 'Nausicaa', 'masculino', 11),
- (null, 'Nausicaa', 'masculino', 11),
- (null, 'Nausicaa', 'masculino', 11);
+ (null, 'Seita', 'masculino', 4),
+ (null, 'Setsuko', 'feminino', 4),
+ (null, 'Jiji', 'criatura mágica', 5),
+ (null, 'Tombo', 'masculino', 5),
+ (null, 'Ursula', 'feminino', 5),
+ (null, 'Kiki', 'feminino', 5),
+ (null, 'Gina', 'feminino', 6),
+ (null, 'Porco Rosso', 'masculino', 6),
+ (null, 'Fio Piccolo', 'feminino', 6),
+ (null, 'Ashitaka', 'masculino', 7),
+ (null, 'San', 'feminino', 7),
+ (null, 'Moro', 'criatura mágica', 7),
+ (null, 'Lady Eboshi', 'feminino', 7),
+ (null, 'Chihiro Ogino', 'feminino', 8),
+ (null, 'Haku', 'masculino', 8),
+ (null, 'Lin', 'feminino', 8),
+ (null, 'Sem Rosto', 'criatura mágica', 8),
+ (null, 'Sophie Hatter', 'feminino', 9),
+ (null, 'Howl', 'masculino', 9),
+ (null, 'Calcifer', 'criatura mágica', 9),
+ (null, 'Cabeça de Nabo', 'criatura mágica', 9),
+ (null, 'Markl', 'masculino', 9),
+ (null, 'Sosuke', 'masculino', 10),
+ (null, 'Ponyo', 'feminino', 10),
+ (null, 'Lisa', 'feminino', 10),
+ (null, 'Fujimoto', 'masculino', 10),
+ (null, 'Granmamare', 'criatura mágica', 10),
+ (null, 'Nahoko Satomi', 'masculino', 11),
+ (null, 'Jiro Horikoshi', 'masculino', 11),
+ (null, 'Honjo', 'masculino', 11);
+ 
+ INSERT INTO Distribuidora VALUES
+(null, 'Toei Company','	¥ 180 milhões', 1),
+(null, 'Sato Company','US$ 15,5milhões', 2),
+(null,'Osaka Company','US$ 45,1milhões', 3),
+(null, 'Toho','US$ 516,9milhões',4),
+(null, 'Tonbo Company','¥ 800 milhões',5),
+(null, 'Tokyo','	US$ 59 milhões',6),
+(null, 'Sapporo','US$ 159,4 milhões',7),
+(null, 'Ginza','US$ 347,7 milhões',8),
+(null, 'Yamanashi', 'US$ 236 milhões',9),
+(null, 'Aomori', 'US$ 202,5 milhões',10),
+(null, 'Chiba', 'US$ 136.454.220', 11);
+
+INSERT INTO Usuario (idUsuario, nomeUser, email, senha, fkFilmes, fkPersonagem) Values
+ (null, 'Gabriela','gabriela@email','123',7 , 23);
+ INSERT INTO Usuario (idUsuario, nomeUser, email, senha, fkFilmes, fkPersonagem) Values
+ (null, 'Akemi','Akemi@email','123',1 , 11),
+ (null, 'victor','victor@email','123',4 , 1),
+ (null, 'maria','maria@email','123',2 , 20),
+ (null, 'lily','lily@email','123',11 , 2),
+ (null, 'poki','poki@email','123',3 , 3),
+ (null, 'julia','julia@email','123',5 , 5),
+ (null, 'silvia','silvia@email','123',6 , 40),
+ (null, 'sergio','sergio@email','123',8 , 32),
+ (null, 'felipe','felipe@email','123',10 , 12),
+ (null, 'leo','leo@email','123',2 , 21),
+ (null, 'yuji','yuji@email','123',1 , 33),
+ (null, 'kenzo','kenzo@email','123',1 , 19),
+ (null, 'namie','namie@email','123',10 , 16),
+ (null, 'luana','luana@email','123',3 , 14),
+ (null, 'kimi','kimi@email','123',3 , 7),
+ (null, 'mana','mana@email','123',1 , 9),
+ (null, 'edu','edu@email','123',5 , 11),
+ (null, 'vini','vini@email','123',5 , 10),
+ (null, 'lilian','lilian@email','123',6 , 3),
+ (null, 'sandra','sandra@email','123',9 , 17);
+ 
+ select 
+        f.nomeFilme as nomeFilme, 
+        count(idUsuario) as top1 
+            from Usuario as u 
+                JOIN Filmes as f 
+                    on u.fkFilmes=f.idFilmes 			
+            group by fkFilmes 
+            order by top1 desc limit 1;
+ select * from Filmes;
+ select * from Personagens;
+ select * from Distribuidora;
+ 
+ select * from Filmes JOIN Personagens ON Filmes.idFilmes = Personagens.ax_filmes JOIN Usuario ON Filmes.idFilmes = Usuario.fkFilmes;
+ select * from Usuario as u JOIN Filmes as f on u.fkFilmes=f.idFilmes JOIN Personagens as p ON u.fkPersonagem=p.idPersonagem WHERE idUsuario = 1;
+ select * from Usuario as u JOIN Filmes as f on u.fkFilmes=f.idFilmes JOIN Personagens as p ON u.fkPersonagem=p.idPersonagem WHERE idUsuario = 1;
+ select Count(u.fkFilmes), Count(u.fkPersonagem) FROM Usuario as u JOIN  Personagens as p ON u.fkPersonagem = p.idPersonagem JOIN Filmes as f ON u.fkFilmes = f.idFilmes Group by nomeFilme;
